20a752c5e66980d3e3eacfc9fabbe25cda913938
LumiNovryMekel <luminovrymekel@gmail.com>
2023-12-30 15:31:01 +0700

    C# Variable

--- C#-Journey/Program.cs
+++ C#-Journey/Program.cs
@@ -2,6 +2,23 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Cara Deklarasi Variable Pada C#
+        int age = 18;
+        double phi = 3.14;
+        char a = 'A';
+        string name = "Lumi Novry Meckel";
+        bool isMarried = false;
+
+        Console.WriteLine("Hai My Name Is " + name);
+        Console.WriteLine("Im " + age + " Years Old");
+        if(isMarried == false)
+        {
+            Console.WriteLine("Lumi Is Not Married");
+        }
+        else
+        {
+            Console.WriteLine("Lumi Is Married");
+        }
+        Console.WriteLine("Nice To Meet You");
     }
 }
