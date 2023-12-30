847858ca3ca0e79baaf20a4827cdf18d1ee0b9b4
LumiNovryMekel <luminovrymekel@gmail.com>
2023-12-31 00:56:05 +0700

    C# Class Encapsulation

--- C#-Journey/Program.cs
+++ C#-Journey/Program.cs
@@ -1,9 +1,15 @@
 ﻿// Create Blueprint/Class
 internal class Manusia
 {
+    // Encapsulation
+    // Private
+    // Protected
+    // Public
     // Deklarasi Property Pada Sebuah Class
     public int umur;
     public string nama;
+
+    public string createdAt = "12/31/2023";
 
     public void SayHi()
     {
@@ -23,5 +29,15 @@ internal class User
         Lumi.umur = 18;
 
         Console.WriteLine(Lumi.nama);
+    }
+}
+
+class Testing
+{
+    static void Main(string[] args)
+    {
+        // Testing
+        Manusia Testing = new Manusia();
+        Console.WriteLine(Testing.createdAt);
     }
 }
