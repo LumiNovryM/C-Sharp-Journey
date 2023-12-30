cb4d3eaa8b51be5a73bcb90d343009fde1de8332
LumiNovryMekel <luminovrymekel@gmail.com>
2023-12-30 15:55:31 +0700

    C# Arithmetic Operation

--- C#-Journey/Program.cs
+++ C#-Journey/Program.cs
@@ -3,27 +3,36 @@
     private static void Main(string[] args)
     {
         // 1.Variable
-        int age = 18;
-        double phi = 3.14;
-        char a = 'A';
-        string name = "Lumi Novry Meckel";
-        bool isMarried = false;
+        // int age = 18;
+        // double phi = 3.14;
+        // char a = 'A';
+        // string name = "Lumi Novry Meckel";
+        // bool isMarried = false;
 
         // Replace Variable Value
-        age = 19;
+        // age = 19;
 
-        Console.WriteLine("Lumi: Hai My Name Is " + name);
-        Console.WriteLine("Lumi: Im " + age + " Years Old");
-        Console.WriteLine("Nice To Meet You");
+        // Console.WriteLine("Lumi: Hai My Name Is " + name);
+        // Console.WriteLine("Lumi: Im " + age + " Years Old");
+        // Console.WriteLine("Nice To Meet You");
 
 
         // 2.User Input
-        Console.WriteLine("Lumi: What is Your Name?");
+        // Console.WriteLine("Lumi: What is Your Name?");
+        // string userName = Console.ReadLine();
+        // Console.WriteLine("{0} : My Name Is {0}", userName);
+        // Console.WriteLine("Lumi: Nice To Meet You, {0}", userName);
 
-        string userName = Console.ReadLine();
-
-        Console.WriteLine("{0} : My Name Is {0}", userName);
-
-        Console.WriteLine("Lumi: Nice To Meet You, {0}", userName);
+        // 3.Aritmatika
+        Console.WriteLine("Masukkan Angka Ke 1 : ");
+        int firstNum = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Masukkang Angka Ke 2 : ");
+        int secondNum = Convert.ToInt32(Console.ReadLine());
+   
+        Console.WriteLine("Hasil Penjumlahan : {0}", firstNum+secondNum);
+        Console.WriteLine("Hasil Pengurangan : {0}", firstNum-secondNum);
+        Console.WriteLine("Hasil Perkalian : {0}",firstNum*secondNum);
+        Console.WriteLine("Hasil Pembagian : {0}", Convert.ToDouble(firstNum) / Convert.ToDouble(secondNum));
+        Console.WriteLine("Hasil Modulus : {0}", firstNum%secondNum);
     }
 }
